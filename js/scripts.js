978f2b0b956de603033c4055ffae64e4b23390c5
Kristen Chellis and Matt Dowdney <matt@>
2018-03-28 11:40:56 -0700

    refactor code using regex

--- js/scripts.js
+++ js/scripts.js
@@ -1,70 +1,57 @@
-var vowels = ["a", "e", "i", "o",
-"u"];
-function vowelWord(string) {
-  for (var i = 0; i < vowels.length; i++) {
-    if (string[0] === vowels[i]) {
-      return true;
-    }else{
+function vowelWord(string){
+  if (/[aeiou]/.test(string[0])){
+    return true;
+  }else {
 
-    }
   }
 }
+
 function consonantWord(string){
   for (var i = 0; i < string.length; i++) {
-    for (var j = 0; j < vowels.length; j++) {
-      if(string[i] === vowels[j]) {
-        if (string[i]=== "u" && string[i-1] === "q") {
-          var slice = string.slice(i+1);
-          var consonants= string.slice(0,i+1);
-          var word = slice + consonants + "ay";
-          j= vowels.length;
-          i= string.length;
-          return word;
-        }else {
-          var slice = string.slice(i);
-          var consonants= string.slice(0,i);
-          var word = slice + consonants + "ay";
-          j= vowels.length;
-          i= string.length;
-          return word;
-        }
+    if (/[aeiou]/.test(string[i])) {
+      if (string[i]=== "u" && string[i-1] === "q") {
+        var slice = string.slice(i+1);
+        var consonants= string.slice(0,i+1);
+        var word = slice + consonants + "ay";
+        i= string.length;
+        return word;
+      }else {
+        var slice = string.slice(i);
+        var consonants= string.slice(0,i);
+        var word = slice + consonants + "ay";
+        i= string.length;
+        return word;
       }
     }
   }
 }
 
-
 function pigLatin(text) {
   text= text.replace(/[.,\/#!$%\^&\*;@:{}=+|\-_"`~()]/g,"");
   text= text.toLowerCase();
   var textArray = text.split(" ");
+  console.log(textArray);
   var latinArray = []
   textArray.forEach(function(word) {
     if (vowelWord(word)) {
       word += "way";
       latinArray.push(word);
+      console.log(latinArray);
     }else {
       word = consonantWord(word);
       latinArray.push(word);
+      console.log(latinArray);
     }
   });
   var finalText = latinArray.join(" ");
   return finalText;
 }
 
-
-
-
-
 $(document).ready(function(){
-
   $(".form1").submit(function(event){
     event.preventDefault();
     var userText = $("#text").val();
     var output = pigLatin(userText);
     $("#output").text(output);
   });
-
-
-
 });
